dfc2aac62284f3c8bb697e640f78d666f0020a64
Kamsby <93309482+Kamsby@users.noreply.github.com>
2022-08-31 13:29:58 +0800

    Encryption.py
    Sample script to create, list and cancel the orders in unified margin.

---
+++ v3_demo/api_demo/unified_margin/Encryption.py
@@ -0,0 +1,61 @@
+import requests
+import time
+import hashlib
+import hmac
+import uuid
+
+api_key='XXXXXXXXXX'
+secret_key='XXXXXXXXXX'
+httpClient=requests.Session()
+recv_window=str(5000)
+url="https://api-testnet.bybit.com" # Testnet endpoint
+
+def HTTP_Request(endPoint,method,payload,Info):
+    global time_stamp
+    time_stamp=str(int(time.time() * 10 ** 3))
+    signature=genSignature(params)
+    headers = {
+        'X-BAPI-API-KEY': api_key,
+        'X-BAPI-SIGN': signature,
+        'X-BAPI-SIGN-TYPE': '2',
+        'X-BAPI-TIMESTAMP': time_stamp,
+        'X-BAPI-RECV-WINDOW': recv_window,
+        'Content-Type': 'application/json'
+    }
+    if(method=="POST"):
+        response = httpClient.request(method, url+endpoint, headers=headers, data=payload)
+    else:
+        response = httpClient.request(method, url+endpoint+"?"+payload, headers=headers)
+    print(response.text)
+    print(Info + " Response Time : " + str(response.elapsed))
+
+def genSignature(payload):
+    param_str= str(time_stamp) + api_key + recv_window + payload
+    hash = hmac.new(bytes(secret_key, "utf-8"), param_str.encode("utf-8"),hashlib.sha256)
+    signature = hash.hexdigest()
+    return signature
+
+#Create Order
+endpoint="/unified/v3/private/order/create"
+method="POST"
+orderLinkId=uuid.uuid4().hex
+params='{"symbol":"BTCUSDT","orderType":"Limit","side":"Buy","qty":"0.001","price":"10000","timeInForce":"GoodTillCancel","category":"linear","orderLinkId": "' + orderLinkId + '"}'
+HTTP_Request(url,method,params,"Create")
+
+#Get unfilled Orders
+endpoint="/unified/v3/private/order/unfilled-orders"
+method="GET"
+params='category=linear&symbol=BTCUSDT'
+HTTP_Request(url,method,params,"UnFilled")
+
+#Get Order List
+endpoint="/unified/v3/private/order/list"
+method="GET"
+params="symbol=BTCUSDT&category=linear&orderLinkId="+orderLinkId
+HTTP_Request(url,method,params,"List")
+
+#Cancel Order
+endpoint="/unified/v3/private/order/cancel"
+method="POST"
+params='{"symbol": "ETHUSDT","category":"linear","orderLinkId": "'+orderLinkId+'"}'
+HTTP_Request(url,method,params,"Cancel")
