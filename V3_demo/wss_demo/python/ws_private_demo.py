b1598f8be25c9b5ab4d18db9d21b827f511b5f85
Kamsby <93309482+Kamsby@users.noreply.github.com>
2022-09-07 14:36:26 +0800

    Update ws_private_demo.py

--- V3_demo/wss_demo/python/ws_private_demo.py
+++ V3_demo/wss_demo/python/ws_private_demo.py
@@ -7,30 +7,22 @@ import websocket
 
 logging.basicConfig(filename='logfile_wrapper.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s')
-topic = "order"
-
-trade_results = {}
-size = {}
-size_usdt = {}
-
+topic = "user.order.contractAccount"
 
 def on_message(ws, message):
     data = json.loads(message)
     print(data)
 
-
 def on_error(ws, error):
     print('we got error')
     print(error)
 
-
 def on_close(ws):
     print("### about to close please don't close ###")
 
-
 def send_auth(ws):
-    key = 'xxxx'
-    secret = 'xxxx'
+    key = 'XXXXXXXXXX'
+    secret = 'XXXXXXXXXX'
     expires = int((time.time() + 10) * 1000)
     _val = f'GET/realtime{expires}'
     print(_val)
@@ -40,17 +32,11 @@ def send_auth(ws):
     ).hexdigest())
     ws.send(json.dumps({"op": "auth", "args": [key, expires, signature]}))
 
-
 def on_pong(ws, *data):
     print('pong received')
 
-
 def on_ping(ws, *data):
-    #now = datetime.now()
-    #dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    #print("date and time =", dt_string)
     print('ping received')
-
 
 def on_open(ws):
     print('opened')
@@ -58,9 +44,8 @@ def on_open(ws):
     print('send subscription ' + topic)
     ws.send(json.dumps({"op": "subscribe", "args": [topic]}))
 
-
 def connWS():
-    ws = websocket.WebSocketApp("wss://stream.bybit.com/realtime_private",
+    ws = websocket.WebSocketApp("wss://stream-testnet.bybit.com/contract/private/v3",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close,
@@ -75,7 +60,6 @@ def connWS():
         ping_timeout=10
     )
 
-
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    #websocket.enableTrace(True)
     connWS()
