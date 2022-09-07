59d242bfb6c401a63d587a8df94fed006ae9fd4d
Kamsby <93309482+Kamsby@users.noreply.github.com>
2022-09-07 14:28:15 +0800

    Update ws_public_demo.js

--- V3_demo/wss_demo/js/ws_public_demo.js
+++ V3_demo/wss_demo/js/ws_public_demo.js
@@ -1,7 +1,7 @@
 var url = require('url');
 var WebSocket = require('ws');
 var crypto = require('crypto');
-var endpoint = "wss://stream-testnet.bybit.com/realtime"
+var endpoint = "wss://stream-testnet.bybit.com/contract/usdt/public/v3"
 console.log('attempting to connect to WebSocket %j', endpoint);
 var client = new WebSocket(endpoint);
 client.on('open', function () {
@@ -9,11 +9,11 @@ client.on('open', function () {
 	console.log('WebSocket Client Connected');
 	setInterval(()=>{client.ping()}, 30000);
 	client.ping();
-	client.send(JSON.stringify({"op": "subscribe", "args": ["orderBookL2_25.BTCUSD"]}));
+	client.send(JSON.stringify({"op": "subscribe", "args": ["orderbook.25.BTCUSDT"]}));
 });
 
 client.on('message', function (data) {
-	console.log('"message" event! %j', data);
+	console.log('"message" event! %j', JSON.parse(Buffer.from(data).toString()));
 });
 client.on('ping', function (data, flags) {
 	console.log("ping received");
