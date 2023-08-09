d652228971e7410a0ad7a37be375b13c18e68df1
wuhewuhe <dachichiwuhe@gmail.com>
2023-08-09 11:54:18 +0200

    add c# HMAC algo signature verification example

---
+++ V5_demo/api_demo/Encryption_HMAC.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Encryption_HMAC
+{
+    public class Encryption
+    {
+        private const string ApiKey = "xxxxxxxxxxxxxx";
+        private const string ApiSecret = "xxxxxxxxxxxxxxxxx";
+        private static readonly string Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString();
+        private const string RecvWindow = "5000";
+
+        public static void Main()
+        {
+            var encryptionTest = new Encryption();
+            encryptionTest.PlaceOrder();
+            // encryptionTest.GetOpenOrder();
+        }
+
+        public void PlaceOrder()
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"category", "linear"},
+                {"symbol", "BTCUSDT"},
+                {"side", "Buy"},
+                {"positionIdx", 0},
+                {"orderType", "Limit"},
+                {"qty", "0.001"},
+                {"price", "18900"},
+                {"timeInForce", "GTC"}
+            };
+
+            var signature = GeneratePostSignature(parameters);
+            var jsonPayload = JsonConvert.SerializeObject(parameters);
+
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api-testnet.bybit.com/v5/order/create")
+            {
+                Content = new StringContent(jsonPayload, Encoding.UTF8, "application/json")
+            };
+
+            request.Headers.Add("X-BAPI-API-KEY", ApiKey);
+            request.Headers.Add("X-BAPI-SIGN", signature);
+            request.Headers.Add("X-BAPI-SIGN-TYPE", "2");
+            request.Headers.Add("X-BAPI-TIMESTAMP", Timestamp);
+            request.Headers.Add("X-BAPI-RECV-WINDOW", RecvWindow);
+
+            var response = client.SendAsync(request).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+
+        private static string GeneratePostSignature(IDictionary<string, object> parameters)
+        {
+            var paramJson = JsonConvert.SerializeObject(parameters);
+            var rawData = Timestamp + ApiKey + RecvWindow + paramJson;
+
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(ApiSecret));
+            var signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            return BitConverter.ToString(signature).Replace("-", "").ToLower();
+        }
+
+        private static string GenerateGetSignature(Dictionary<string, string> parameters)
+        {
+            var rawData = Timestamp + ApiKey + RecvWindow + string.Join("", parameters.Select(p => p.Key + p.Value));
+            return ComputeSignature(rawData);
+        }
+
+        private static string ComputeSignature(string data)
+        {
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(ApiSecret));
+            byte[] signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(signature).Replace("-", "").ToLower();
+        }
+
+        private static string GenerateQueryString(Dictionary<string, string> parameters)
+        {
+            return string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+        }
+    }
+}
