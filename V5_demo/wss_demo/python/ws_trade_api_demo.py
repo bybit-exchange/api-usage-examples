9ecd136d2bcdac12368801a30f91dd88306ccc6a
wuhewuhe <dachichiwuhe@gmail.com>
2024-06-03 13:17:34 +0200

    add ping pong

--- V5_demo/wss_demo/python/ws_trade_api_demo.py
+++ V5_demo/wss_demo/python/ws_trade_api_demo.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 
 async def authenticate(ws):
-    api_key = "xxxxxx"
-    api_secret = "xxxxxxxxxxx"
+    api_key = "xxxxxxxxxx"
+    api_secret = "xxxxxxxxxx"
     expires = int((datetime.now().timestamp() + 10) * 1000)  # Expires in 10 seconds
     signature = hmac.new(api_secret.encode(), f'GET/realtime{expires}'.encode(), hashlib.sha256).hexdigest()
 
@@ -25,7 +25,7 @@ async def authenticate(ws):
 
 async def send_subscription(ws):
     sub_msg = {
-        "reqId": "sub-001",
+        "reqId": "sub-003",
         "header": {
             "X-BAPI-TIMESTAMP": str(int(datetime.now().timestamp() * 1000)),
             "X-BAPI-RECV-WINDOW": "8000",
@@ -33,11 +33,16 @@ async def send_subscription(ws):
         "op": "order.create",
         "args": [
             {
-                "symbol": "XRPUSDT",
-                "side": "Buy",
+                "category": "linear",
+                "orderLinkId": "crzyO2.0-1716303565222835550",
                 "orderType": "Market",
-                "qty": "10",
-                "category": "spot"
+                "positionIdx": 1,
+                "price": "0.5496",
+                "qty": "11",
+                "side": "Buy",
+                "symbol": "XRPUSDT",
+                "reduceOnly": False,
+                "closeOnTrigger": False
             }
         ]
     }
@@ -46,10 +51,24 @@ async def send_subscription(ws):
     print("Subscription message sent.")
 
 
-async def listen_messages(ws):
+async def on_ping(ws):
     while True:
-        message = await ws.recv()
-        print(f"Message received: {message}")
+        try:
+            print('Sending ping')
+            await ws.send(json.dumps({'op': 'ping'}))
+            await asyncio.sleep(10)  # Sleep for 10 seconds
+        except websockets.exceptions.ConnectionClosed:
+            print("WebSocket connection closed.")
+            break
+
+
+async def listen_messages(ws):
+    try:
+        while True:
+            message = await ws.recv()
+            print(f"Message received: {message}")
+    except websockets.exceptions.ConnectionClosed:
+        print("WebSocket connection closed.")
 
 
 async def main():
@@ -57,7 +76,9 @@ async def main():
     async with websockets.connect(uri) as ws:
         await authenticate(ws)
         await send_subscription(ws)
-        await listen_messages(ws)
+        listen_task = asyncio.create_task(listen_messages(ws))
+        ping_task = asyncio.create_task(on_ping(ws))
+        await asyncio.gather(listen_task, ping_task)
 
 
 # Run the main function in an asyncio event loop
